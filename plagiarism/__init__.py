273e6ea04cb5a2ab33e817d0bdf67c76f65e954b
Natworpong Loyswai <28857628+rsxss@users.noreply.github.com>
2021-09-21 08:46:38 +0700

    FEAT moss checker

---
+++ plagiarism/__init__.py
@@ -0,0 +1,45 @@
+import pathlib
+import mosspy
+
+_id = 731376226
+
+
+class Moss(mosspy.Moss):
+
+    def __init__(self, *args, base_path='bases', sample_path='samples', **kwargs):
+        self.base_path = pathlib.Path(base_path)
+        self.sample_path = pathlib.Path(sample_path)
+        super().__init__(*args, **kwargs)
+
+    def addBaseFile(self, file_path, display_name=None):
+        file_path = str(self.base_path / pathlib.Path(file_path))
+        super().addBaseFile(file_path, display_name)
+
+    def addFile(self, file_path, display_name=None):
+        file_path = str(self.sample_path / pathlib.Path(file_path))
+        super().addFile(file_path, display_name)
+
+    def addFilesByWildcard(self, wildcard):
+        file_path = str(self.sample_path / pathlib.Path(wildcard))
+        super().addFilesByWildcard(file_path)
+
+
+if __name__ == '__main__':
+    def report_callback(file_path, display_name):
+        print('file_path:', file_path)
+        print('display_name:', display_name)
+        # print('*', end='', flush=True)
+
+
+    _moss = Moss(_id, 'python')
+    _moss.addBaseFile('base_0.txt')
+    _moss.addBaseFile('base_1.txt')
+    _moss.addFilesByWildcard('sample_*.py')
+
+    report_url = _moss.send(report_callback)
+    print(report_url)
+
+    # Save report file
+    _moss.saveWebPage(report_url, "report.html")
+    mosspy.download_report(report_url, "report_w.html", connections=8, log_level=10,
+                           on_read=lambda url: print('*', end='', flush=True))
---
+++ plagiarism/samples/sample_1.py
@@ -0,0 +1,13 @@
+# Content inserted by instructors
+# Instruction-0: Write a star of yours to the output
+# Instruction-1: Write a square of yours to the output
+# Instruction-2: Write a string "ending program" to the output and end the program
+
+# Self introduction
+print('Hello World! i\'m', __name__)
+# Instruction-0: Write a star of yours to the output
+print('*')
+# Instruction-1: Write a square of yours to the output
+print('square')
+# Instruction-2: Write a string "ending program" to the output and end the program
+print('ending program')
